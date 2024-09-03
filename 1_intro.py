9568b76a8a962a7e298cd8e69da0984beef581c4
selosinambela <chelloaja47@gmail.com>
2024-09-03 12:38:02 +0700

    perkuliahan_1

---
+++ 1_intro.py
@@ -0,0 +1,20 @@
+# Dekalrasi variabel
+a = float(10.5)
+b = 5
+c = a + b
+print("Hello World")
+
+# Operasi (operation) -> tambah (+), kurang, (-)bagi(/), kali (*), Modulo(%), pangkat(**)
+a = 10
+b = 4
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
